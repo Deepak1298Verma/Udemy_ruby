f2a1518e8ffcb66465407664ca8105c30d037844
Deepak Verma <92680012+Deepak1298Verma@users.noreply.github.com>
2023-01-08 10:21:52 +0000

    Assignment_1

--- main.rb
+++ main.rb
@@ -1,5 +1,35 @@
-def show(things_to_say)
-  puts things_to_say
-end
+# def show(things_to_say)
+#   puts things_to_say
+# end
 
-show("hello world!!!")
+# show('hello world!!!')
+
+# # String interpolation
+# first = 'Deepak'
+# last = 'Verma'
+# full_name = first + ' ' + last
+# puts full_name
+
+# sentense = 'Welcome to the jungle'
+# puts sentense
+# puts sentense.sub('the jungle', 'the park')
+
+# puts
+
+# name = 'Deepak Verma'
+# puts name
+# new_name = name
+
+# name = 'Name changed!!!'
+
+# puts name
+# puts new_name
+
+# taking User input
+# puts 'What is you first name ?'
+# name = gets.chomp
+# puts "Thank You, your name is #{name}"
+
+puts "Enter the number please ?"
+integer = gets.chomp.to_i
+puts integer * 2
